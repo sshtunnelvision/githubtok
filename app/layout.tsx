15e809e297301056ba2f0f1390a2e88cd2847105
arekhalpern <arek.halpern@gmail.com>
2025-02-11 14:39:02 -0500

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/nav-bar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-full`}>
         <NavBar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
